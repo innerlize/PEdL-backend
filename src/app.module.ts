ee371472007dcf932948e4feb36126ac823ee77e
innerlize <alan.dev@outlook.kr>
2024-06-14 17:05:22 -0300

    Import new module to App module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { FirebaseModule } from './modules/firebase/firebase.module';
 import { ConfigModule } from '@nestjs/config';
 
 @Module({
@@ -8,6 +9,7 @@ import { ConfigModule } from '@nestjs/config';
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    FirebaseModule,
   ],
   controllers: [AppController],
   providers: [AppService],
