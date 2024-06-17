88ecd60c93090c3ef58ab8d19d4a7a3d100d1a2e
innerlize <alan.dev@outlook.kr>
2024-06-16 23:06:38 -0300

    Modify 'Database' repository interface

--- src/common/domain/database-repository.interface.ts
+++ src/common/domain/database-repository.interface.ts
@@ -1,7 +1,7 @@
 export interface DatabaseRepository<T> {
   findAll(collectionName: string): Promise<T[]>;
   findById(collectionName: string, id: string): Promise<T>;
-  create(collectionName: string, data: T): Promise<string>;
-  update(collectionName: string, id: string, data: T): Promise<string>;
+  create(collectionName: string, data: any): Promise<string>;
+  update(collectionName: string, id: string, data: any): Promise<string>;
   delete(collectionName: string, id: string): Promise<string>;
 }
