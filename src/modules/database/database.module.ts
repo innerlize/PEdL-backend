db58594b627af05c8bd6110aec056236d4da876d
innerlize <alan.dev@outlook.kr>
2024-06-26 20:55:52 -0300

    Setup new Firebase config for each environment and create 'firebase-config' file

--- src/modules/database/database.module.ts
+++ src/modules/database/database.module.ts
@@ -1,23 +1,12 @@
 import { Module } from '@nestjs/common';
-import * as admin from 'firebase-admin';
 import { FirebaseRepository } from './infrastructure/repositories/firebase.repository';
-import { ConfigService } from '@nestjs/config';
+import { getFirebaseConfig } from '../../common/config/get-firebase-config';
 
 @Module({
   providers: [
     {
       provide: 'FirebaseAdmin',
-      useFactory: (configService: ConfigService) => {
-        // eslint-disable-next-line @typescript-eslint/no-var-requires
-        const serviceAccount = require(
-          configService.get<string>('PATH_TO_SERVICE_ACCOUNT_KEY'),
-        );
-        const app = admin.initializeApp({
-          credential: admin.credential.cert(serviceAccount),
-        });
-        return app;
-      },
-      inject: [ConfigService],
+      useFactory: getFirebaseConfig,
     },
     {
       provide: 'DatabaseRepository',
