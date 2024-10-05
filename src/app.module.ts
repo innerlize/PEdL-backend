14f3d955dd0cacf336e576bb1a6844e7329d1431
innerlize <alan.dev@outlook.kr>
2024-10-04 23:31:07 -0300

    Import new 'Auth' module to root module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { DatabaseModule } from './modules/database/database.module';
 import { ConfigModule } from '@nestjs/config';
 import { ProjectsModule } from './modules/projects/projects.module';
 import { PartnersModule } from './modules/partners/partners.module';
+import { AuthModule } from './modules/auth/auth.module';
 import { FirebaseAdminModule } from './modules/firebase/firebase-admin.module';
 
 @Module({
@@ -17,6 +18,7 @@ import { FirebaseAdminModule } from './modules/firebase/firebase-admin.module';
     DatabaseModule,
     ProjectsModule,
     PartnersModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
