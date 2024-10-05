9753e7ff1c2159d6229a7748fbfa53e450337b59
innerlize <alan.dev@outlook.kr>
2024-10-04 23:17:04 -0300

    Create 'FirebaseAuth' service

---
+++ src/modules/auth/application/services/firebase-auth.service.ts
@@ -0,0 +1,47 @@
+import { Inject, Injectable } from '@nestjs/common';
+import * as admin from 'firebase-admin';
+import { AuthService } from '../../../../common/domain/auth-service.interface';
+
+@Injectable()
+export class FirebaseAuthService implements AuthService {
+  private auth: admin.auth.Auth;
+
+  constructor(
+    @Inject('FirebaseAdmin') private readonly firebaseAdmin: admin.app.App,
+  ) {
+    this.auth = this.firebaseAdmin.auth();
+  }
+
+  private async verifyAndDecodeFirebaseToken(token: string): Promise<any> {
+    try {
+      const decodedToken = await this.auth.verifyIdToken(token, true);
+
+      return decodedToken;
+    } catch (error) {
+      return null;
+    }
+  }
+
+  async verifyAdminAccess(token: string): Promise<boolean> {
+    try {
+      const userData = await this.verifyAndDecodeFirebaseToken(token);
+
+      const isExpectedUser = userData.email === process.env.ADMIN_EMAIL;
+      const userEmailIsVerified = userData.email_verified;
+
+      if (isExpectedUser && userEmailIsVerified) {
+        return true;
+      }
+
+      return false;
+    } catch (error) {
+      return false;
+    }
+  }
+
+  async revokeToken(token: string): Promise<void> {
+    const { uid } = await this.verifyAndDecodeFirebaseToken(token);
+
+    await this.auth.revokeRefreshTokens(uid);
+  }
+}
