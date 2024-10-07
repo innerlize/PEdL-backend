b77b80ab1c1c6908fe2bba2c2701eb0c545a3936
innerlize <alan.dev@outlook.kr>
2024-10-07 18:21:43 -0300

    Add test cases for failure scenarios in FirebaseAuthService

--- src/modules/auth/application/services/__tests__/firebase-auth.service.spec.ts
+++ src/modules/auth/application/services/__tests__/firebase-auth.service.spec.ts
@@ -69,10 +69,57 @@ describe('FirebaseAuthService', () => {
       });
   });
 
-  it('should fail to validate token and verify admin user', async () => {
+  it('should fail to verify admin user if user is not the expected one', async () => {
+    const mockedUid = 'def456';
+    const mockedEmail = 'L8QpG@example.com';
+
+    const userRecord = await createUser(mockedUid, mockedEmail);
+
+    const customToken = await generateCustomToken(userRecord.uid);
+
+    const { idToken } = await verifyCustomToken(customToken);
+
+    await updateUserEmail(userRecord.uid, mockedEmail);
+
     await request(app.getHttpServer())
       .post('/api/admin/auth/verify-admin-access')
-      .set('Authorization', 'Bearer randomToken')
+      .set('Authorization', `Bearer ${idToken}`)
+      .then((res) => {
+        expect(res.status).toBe(401);
+        expect(res.ok).toBeFalsy();
+      });
+  });
+
+  it('should fail to verify admin user if user email is not verified', async () => {
+    const mockedUid = 'abc123';
+    const mockedEmail = process.env.ADMIN_EMAIL as string;
+    const mockedIsEmailVerified = false;
+
+    const userRecord = await createUser(
+      mockedUid,
+      mockedEmail,
+      mockedIsEmailVerified,
+    );
+
+    const customToken = await generateCustomToken(userRecord.uid);
+
+    const { idToken } = await verifyCustomToken(customToken);
+
+    await request(app.getHttpServer())
+      .post('/api/admin/auth/verify-admin-access')
+      .set('Authorization', `Bearer ${idToken}`)
+      .then((res) => {
+        expect(res.status).toBe(401);
+        expect(res.ok).toBeFalsy();
+      });
+  });
+
+  it('should fail to verify admin user if token is not valid', async () => {
+    const randomToken = 'cR4zY4nDraNdOmT0k3n';
+
+    await request(app.getHttpServer())
+      .post('/api/admin/auth/verify-admin-access')
+      .set('Authorization', `Bearer ${randomToken}`)
       .then((res) => {
         expect(res.status).toBe(401);
         expect(res.ok).toBeFalsy();
