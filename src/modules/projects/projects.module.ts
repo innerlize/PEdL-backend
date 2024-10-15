1864fa039ec2513da40d04cf522b7fa5628906a2
innerlize <alan.dev@outlook.kr>
2024-10-15 04:47:04 -0300

    Import 'Auth' module into 'Partners' and 'Projects' modules

--- src/modules/projects/projects.module.ts
+++ src/modules/projects/projects.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { ProjectsService } from './application/services/projects.service';
 import { DatabaseModule } from '../database/database.module';
 import { ProjectsController } from './controllers/projects.controller';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [AuthModule, DatabaseModule],
   providers: [ProjectsService],
   controllers: [ProjectsController],
 })
