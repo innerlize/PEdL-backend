058493570719cac9ca1ba9feebe8ae7fe460e8f3
innerlize <alan.dev@outlook.kr>
2024-10-20 23:13:16 -0300

    Initialize Swagger config and builder in 'main.ts' file

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('PEdL API')
+    .setDescription(
+      'A highly scalable and maintainable API for managing various resources, with secure admin user authentication and a comprehensive set of features for efficient content management.',
+    )
+    .setVersion('1.0')
+    .build();
 
   app.enableCors();
 
@@ -13,6 +22,10 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
+
+  const apiDocument = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api/docs', app, apiDocument);
 
   await app.listen(3808);
 }
