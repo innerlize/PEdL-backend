546f6ae9309cda15adf5e27c405bfcf696ec0406
innerlize <alan.dev@outlook.kr>
2024-11-01 21:12:23 -0300

    Import 'Storage' module into 'Projects' module

--- src/modules/projects/projects.module.ts
+++ src/modules/projects/projects.module.ts
@@ -4,9 +4,10 @@ import { DatabaseModule } from '../database/database.module';
 import { ProjectsController } from './controllers/projects.controller';
 import { AuthModule } from '../auth/auth.module';
 import { ProjectsOrderService } from './application/services/projects-order.service';
+import { StorageModule } from '../storage/storage.module';
 
 @Module({
-  imports: [AuthModule, DatabaseModule],
+  imports: [AuthModule, DatabaseModule, StorageModule],
   providers: [ProjectsService, ProjectsOrderService],
   controllers: [ProjectsController],
 })
